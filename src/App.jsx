9f2c1cbf18915cb5e19f8cf2c27ad2c3dc38a92e
cirjeudumitru-prog <cirjeu.dumitru@elev.cihcahul.md>
2026-06-28 07:56:36 +0300

    firs part of the hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import React from "react";
-import { gsap } from "gsap"; 
-import { ScrollTrigger , SplitText} from "gsap/all"; 
-import { useGSAP } from "@gsap/react"; 
+import { gsap } from "gsap";
+import { ScrollTrigger, SplitText } from "gsap/all";
+import { useGSAP } from "@gsap/react";
 import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
 gsap.registerPlugin(ScrollTrigger, SplitText)
 
-const App=()=>{
+const App = () => {
 
-    return( 
+    return (
         <main>
-                <Navbar/>
+            <Navbar />
+            <Hero />
+            <div className="h-dvh"></div>
         </main>
     )
 }
